d5ca38f56b1fc0d4955f5982fc12b7d8082f6397
Dasha <dasha@r70.ru>
2022-04-05 03:29:43 +0700

    characters outline

---
+++ Assets/Game/Highlight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highlight : MonoBehaviour
+{
+    public GameObject _highlightOn;
+    public Image _characterImage;
+    public CampSystem _campSystem;
+
+    private Dictionary<string, string> _charactersNameDict = new Dictionary<string, string>()
+    {
+        { "DadImage", "Dad" },
+        { "MomImage", "Mom" },
+        { "KidImage", "Kid" }
+    };
+
+    public void HandleMouseEnter()
+    {
+        string characterName = _charactersNameDict[_characterImage.name];
+        if (
+            _characterImage.IsActive()
+            && _campSystem.Characters[characterName].IsAlive
+            && !_campSystem.Characters[characterName].IsFed
+            && _campSystem.CurrentFood >= _campSystem.Characters[characterName].FoodRequired
+        )
+        {
+            _highlightOn.SetActive(true);
+        }
+    }
+}
